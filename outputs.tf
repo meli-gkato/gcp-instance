661b67c2f939eeab1f09c200e591d39f3d6f9f40
Gustavo Kato <gustavo.kato@mercadolivre.com>
2022-08-25 01:40:03 -0300

    feat: set outputs

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "name" {
+  value = google_compute_instance.this[*].name
+  description = "Nome da VM"
+}
+
+output "instance_id" {
+  value = google_compute_instance.this[*].instance_id
+  description = "ID da VM"
+}
+
+output "external_ip" {
+  value = google_compute_instance.this[*].network_interface.0.access_config.0.nat_ip
+  description = "IP externo"
+}
+
+output "internal_ip" {
+  value = google_compute_instance.this[*].network_interface.0.access_config.0.network_ip
+  description = "IP interno"
+}
