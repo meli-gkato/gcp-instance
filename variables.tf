510feee1bc728dbbdc3dd11ce6148d10d9e71f23
Gustavo Kato <gustavo.kato@mercadolivre.com>
2022-08-25 01:20:32 -0300

    feat: first module

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "amount" {
+  description = "Quantidade de máquinas desejadas"
+  type        = number
+  default     = 1
+}
+
+variable "name" {
+  description = "Nome da instância"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Tamanho da intância"
+  type        = string
+  default     = "f1-micro"
+}
+
+variable "zone" {
+  description = "Em qual zona sua instância irá ficar"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "image" {
+  description = "Qual sistema deseja utilizar"
+  type        = string
+  default     = "debian-cloud/debian-10"
+}
+variable "network" {
+  description = "Qual rede deseja utilizar"
+  type        = string
+  default     = "default"
+}
